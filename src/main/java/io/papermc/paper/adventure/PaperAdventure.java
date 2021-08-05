3c21729bcf21f5dfa63b2e757360ddcab0f665fe
Romaindu35 <67557705+Romaindu35@users.noreply.github.com>
2021-08-05 18:18:42 +0200

    Prevent error encoding packet

--- src/main/java/io/papermc/paper/adventure/PaperAdventure.java
+++ src/main/java/io/papermc/paper/adventure/PaperAdventure.java
@@ -155,7 +155,8 @@ public final class PaperAdventure {
     }
 
     public static ITextComponent asVanilla(final Component component) {
-        if (true) return new AdventureComponent(component);
+        // Mohist : prevent error encoding packet 
+        //if (true) return new AdventureComponent(component); TODO: correct fix bug
         return ITextComponent.Serializer.fromJson(GSON.serializer().toJsonTree(component));
     }
 
